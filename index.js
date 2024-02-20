e20303d7fd6d9c9dd409f2428415ee7a440adc1c
raj-meena <raj.meena@hotmail.com>
2024-02-20 20:53:31 +0530

    server

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-import express from "express";
-
-const app = express();
-const port = 9000;
-app.use("/", (req, res) => {
-  res.json({ message: "Hello From Express App" });
-});
-
-app.listen(9000, () => {
-  console.log(`Starting Server on Port ${port}`);
-});
-
-// server.js
 // import express from "express";
-// const cors = require("cors");
-// const orderRoutes = require("./routes/orderRoutes").default;
 
 // const app = express();
+// const port = 9000;
+// app.use("/", (req, res) => {
+//   res.json({ message: "Hello From Express App" });
+// });
 
-// app.use(cors());
-// app.use(express.json());
-// app.use("/api", orderRoutes);
+// app.listen(9000, () => {
+//   console.log(`Starting Server on Port ${port}`);
+// });
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// server.js
+import express, { json } from "express";
+import cors from "cors";
+import orderRoutes from "./routes/orderRoutes";
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use("/api", orderRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
